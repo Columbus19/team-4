2fdb7930862ab129aba91c767dafefb389d98662
Alejandro <rodrigueznatalandy@gmail.com>
2019-10-18 23:40:32 -0400

    Added routes for auth (login, register)

---
+++ webapp/flaskr/auth.py
@@ -0,0 +1,68 @@
+import functools
+
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+blue_print = Blueprint('auth', __name__, url_prefix='/auth')
+
+@blue_print.route('/register',methods=('GET', 'POST'))
+def register():
+    if request.method ='POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        db = get_db()
+        error = None
+
+    if not username:
+        error ='Username is required'
+
+    if not password:
+        error = 'Password is required'
+    
+    if db.execute(
+        'SELECT id FROM user WHERE username = ?', (username,)
+    ).fetchone() is not None:
+        error = 'User {} is already registered.'.format(username)
+
+    if error is None:
+                db.execute(
+                    'INSERT INTO user (username, password) VALUES (?, ?)',
+                    (username, generate_password_hash(password))
+                )
+                db.commit()
+                return redirect(url_for('auth.login'))
+
+    flash(error)
+
+    return render_template('auth/register.html')
+
+
+@blue_print.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        db =get_db()
+        error = None
+
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Invalid Username.'
+        
+        if note check_password_hash(user['password'], password):
+            error = 'Incorrect Password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] =user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+    return render_template('auth/login.html')
